3e81d995b0f9d3e262102a803abc94e389a18946
James Strain <jstrain@nvidia.com>
2026-08-07 08:13:12 +1000

    feat: add Haskell syntax highlighting

--- packages/highlight/src/__tests__/parsers.test.ts
+++ packages/highlight/src/__tests__/parsers.test.ts
@@ -22,6 +22,7 @@ describe("isLanguageSupported", () => {
     expect(isLanguageSupported("test.dart")).toBe(true);
     expect(isLanguageSupported("test.cs")).toBe(true);
     expect(isLanguageSupported("test.nix")).toBe(true);
+    expect(isLanguageSupported("test.hs")).toBe(true);
     expect(isLanguageSupported("test.ex")).toBe(true);
   });
 
@@ -62,6 +63,7 @@ describe("getSupportedExtensions", () => {
     expect(extensions).toContain("dart");
     expect(extensions).toContain("cs");
     expect(extensions).toContain("nix");
+    expect(extensions).toContain("hs");
     expect(extensions).toContain("json");
   });
 });
